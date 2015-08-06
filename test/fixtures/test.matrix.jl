ef25d9e6664a3915b109de62a693150fae07cefc
Planeshifter <pburckhardt@outlook.com>
2015-08-07 00:28:37 +0200

    [INIT]

---
+++ test/fixtures/test.matrix.jl
@@ -0,0 +1,22 @@
+using Distributions
+using JSON
+
+k = 1
+d = Chisq( k )
+
+x = linspace( 1, 0, 25 )
+
+dmgf(t) = mgf(d, t )
+y = map( dmgf, x )
+println( y )
+
+data = Dict([
+	("k", k),
+	("data", x),
+	("expected", y)
+])
+
+outfile = open("./test/fixtures/matrix.json", "w")
+JSON.json(data)
+
+write( outfile, JSON.json(data) )
---
+++ test/fixtures/test.partial.jl
@@ -0,0 +1,22 @@
+using Distributions
+using JSON
+
+k = 1
+d = Chisq( k )
+
+x = [ -5, -2.5, 0, 2.5, 5 ]
+
+dmgf(t) = mgf(d, t )
+y = map( dmgf, x )
+println( y )
+
+data = Dict([
+	("k", k),
+	("data", x),
+	("expected", y)
+])
+
+outfile = open("./test/fixtures/partial.json", "w")
+JSON.json(data)
+
+write( outfile, JSON.json(data) )
